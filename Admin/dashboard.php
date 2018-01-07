31914c496332b4dc9285d98de8957ee6e2d502ee
Tuyen Nguyen <ntctuyen.ctu@gmail.com>
2018-01-07 16:20:20 +0700

    chỉnh sửa menu

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -50,10 +50,6 @@
 								<li><a href="#"> <i class="fa fa-tachometer"></i> Bảng điều khiển</a></li>
 								<li>
 									<a href="?page=category"><i class="fa fa-cogs" aria-hidden="true"></i> Danh mục </a> 
-									<ul class="gn-submenu">
-										<li class="mini_list_agile"><a href="buttons.html"><i class="fa fa-caret-right" aria-hidden="true"></i> Buttons</a></li>
-										<li class="mini_list_w3"><a href="grids.html"> <i class="fa fa-caret-right" aria-hidden="true"></i> Grids</a></li>
-									</ul>
 								</li>
 								<li>
 									<a href="?page=publisher"> <i class="fa fa-file-text-o" aria-hidden="true"></i>Publisher <i class="fa fa-angle-down" aria-hidden="true"></i></a> 
@@ -63,10 +59,12 @@
 									</ul>
 								</li>
 								<li><a href="table.html"> <i class="fa fa-table" aria-hidden="true"></i> Tables</a></li>
-								<li><a href="#"><i class="fa fa-list" aria-hidden="true"></i>Short Codes <i class="fa fa-angle-down" aria-hidden="true"> </i></a> 
+								<li><a href="#"><i class="fa fa-list" aria-hidden="true"></i>Quản lý sản phẩm<i class="fa fa-angle-down" aria-hidden="true"> </i></a> 
 									<ul class="gn-submenu">
-										<li class="mini_list_agile"><a href="typo.html"><i class="fa fa-caret-right" aria-hidden="true"></i> Typography</a></li>
-										<li class="mini_list_w3"><a href="icons.html"> <i class="fa fa-caret-right" aria-hidden="true"></i> Icons</a></li>
+										<li class="mini_list_agile"><a href="typo.html"><i class="fa fa-caret-right" aria-hidden="true"></i> Quản lỷ rượu</a></li>
+										<li class="mini_list_w3"><a href="icons.html"> <i class="fa fa-caret-right" aria-hidden="true"></i> Quản lý nhà sản xuất</a></li>
+										<li class="mini_list_agile"><a href="typo.html"><i class="fa fa-caret-right" aria-hidden="true"></i> Quản lỷ xuất xứ</a></li>
+										<li class="mini_list_w3"><a href="icons.html"> <i class="fa fa-caret-right" aria-hidden="true"></i> Quản lý khuyến mãi</a></li>
 										
 									</ul>
 								</li>
