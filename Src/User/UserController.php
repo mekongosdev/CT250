f60274e1e17649e5cb89c8afc049a9dbd643bd95
Huy <huyb1505883@student.ctu.edu.vn>
2018-01-13 17:03:49 +0700

    Thêm UserController và Register

---
+++ Src/User/UserController.php
@@ -0,0 +1,9 @@
+<?php
+function addUser($username, $password,$fullname,$sex,$address,$phone,$email,$dayofbirth)
+{
+	$insert = "INSERT INTO 
+	`User`(`Username`, `Password`, `FullName`, `Sex`, `Address`, `Phone`, `Email`, `DateOfBirth`, `IC`, `Active`, `Status`, `Role`) 
+	VALUES ('$username', '".md5($password)."', '$fullname', '$sex','$address', '$phone', '$email', '$dayofbirth', '$ic', 0, 0,0)";
+	mysql_query($insert);
+}
+?>
