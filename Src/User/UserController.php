d48bd493e25a01b709a5abe3c836ed4578649139
Tuyen Nguyen <ntctuyen.ctu@gmail.com>
2018-01-13 18:13:14 +0700

    Đăng ký tài khoản

--- Src/User/UserController.php
+++ Src/User/UserController.php
@@ -1,9 +1,9 @@
 <?php
-function addUser($username, $password,$fullname,$sex,$address,$phone,$email,$dayofbirth)
+function addUser($username, $password,$fullname,$sex,$address,$phone,$email,$dayofbirth,$identitycard)
 {
 	$insert = "INSERT INTO 
 	`User`(`Username`, `Password`, `FullName`, `Sex`, `Address`, `Phone`, `Email`, `DateOfBirth`, `IC`, `Active`, `Status`, `Role`) 
-	VALUES ('$username', '".md5($password)."', '$fullname', '$sex','$address', '$phone', '$email', '$dayofbirth', '$ic', 0, 0,0)";
+	VALUES ('$username', '".md5($password)."', '$fullname', '$sex','$address', '$phone', '$email', '$dayofbirth', '$identitycard', 1, 1,1)";
 	mysql_query($insert);
 }
 ?>
