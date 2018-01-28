50e4a0d2bb54061c4c6ba4a04f61888bbd1cccac
Tuyen Nguyen <ntctuyen.ctu@gmail.com>
2018-01-28 12:57:57 +0700

    thay đổi hiệu ứng menu

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -64,27 +64,27 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 									<a href="#"> <i class="fa fa-file-text-o" aria-hidden="true"></i>Hóa đơn</a>
 								</li>
 								<li><a href="#"> <i class="fa fa-table top" aria-hidden="true"></i>Phản hồi</a></li>
-								<li><a href="#"><i class="fa fa-list" aria-hidden="true"></i>Quản lý sản phẩm<i class="fa fa-angle-down" aria-hidden="true"> </i></a>
+								<li class="product"><a href="#"><i class="fa fa-list" aria-hidden="true"></i>Quản lý sản phẩm<i class="fa fa-angle-down" aria-hidden="true"> </i></a>
 									<ul class="gn-submenu">
-<li class="mini_list_agile"><a href="?page=category"><i class="fa fa-caret-right" aria-hidden="true"></i> Nhà sản xuất</a></li>
-<li class="mini_list_w3"><a href="?page=publisher"> <i class="fa fa-caret-right" aria-hidden="true"></i> Loại sản phẩm</a></li>
-<li class="mini_list_agile"><a href="?page=country"><i class="fa fa-caret-right" aria-hidden="true"></i> Xuất xứ</a></li>
-<li class="mini_list_w3"><a href="?page=promotion"> <i class="fa fa-caret-right" aria-hidden="true"></i> Khuyến mãi</a></li>
-<li class="mini_list_agile"><a href="?name=wine"><i class="fa fa-caret-right" aria-hidden="true"></i> Quản lý rượu</a></li>
+										<li class="mini_list_agile"><a href="?page=category"><i class="fa fa-caret-right" aria-hidden="true"></i> Nhà sản xuất</a></li>
+										<li class="mini_list_w3"><a href="?page=publisher"> <i class="fa fa-caret-right" aria-hidden="true"></i> Loại sản phẩm</a></li>
+										<li class="mini_list_agile"><a href="?page=country"><i class="fa fa-caret-right" aria-hidden="true"></i> Xuất xứ</a></li>
+										<li class="mini_list_w3"><a href="?page=promotion"> <i class="fa fa-caret-right" aria-hidden="true"></i> Khuyến mãi</a></li>
+										<li class="mini_list_agile"><a href="?name=wine"><i class="fa fa-caret-right" aria-hidden="true"></i> Quản lý rượu</a></li>
 
-<li class="mini_list_w3"><a href="?page=paymentmethod"> <i class="fa fa-caret-right" aria-hidden="true"></i> Hình thức thanh toán</a></li>
+										<li class="mini_list_w3"><a href="?page=paymentmethod"> <i class="fa fa-caret-right" aria-hidden="true"></i> Hình thức thanh toán</a></li>
 
 									</ul>
 								</li>
 
-<li class="page"><a href="#"><i class="fa fa-files-o" aria-hidden="true"></i> Quản lý nhân viên <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+								<li class="employee"><a href="#"><i class="fa fa-files-o" aria-hidden="true"></i> Quản lý nhân viên <i class="fa fa-angle-down" aria-hidden="true"></i></a>
 									<ul class="gn-submenu">
 
-<li class="mini_list_agile"> <a href="#"> <i class="fa fa-caret-right" aria-hidden="true"></i> Thông tin nhân viên</a></li>
-<li class="mini_list_w3"><a href="#"> <i class="fa fa-caret-right" aria-hidden="true"></i> Tin tức</a></li>
-<li class="mini_list_agile error"><a href="#"> <i class="fa fa-caret-right" aria-hidden="true"></i> Chi nhánh </a></li>
+										<li class="mini_list_agile"> <a href="#"> <i class="fa fa-caret-right" aria-hidden="true"></i> Thông tin nhân viên</a></li>
+										<li class="mini_list_w3"><a href="#"> <i class="fa fa-caret-right" aria-hidden="true"></i> Tin tức</a></li>
+										<li class="mini_list_agile error"><a href="#"> <i class="fa fa-caret-right" aria-hidden="true"></i> Chi nhánh </a></li>
 
-<li class="mini_list_w3_line"><a href="?page=role"> <i class="fa fa-caret-right" aria-hidden="true"></i> Quyền hạn</a></li>
+										<li class="mini_list_w3_line"><a href="?page=role"> <i class="fa fa-caret-right" aria-hidden="true"></i> Quyền hạn</a></li>
 									</ul>
 								</li>
 							</ul>
@@ -613,7 +613,7 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 		<!--copy rights end here-->
 		<!-- js -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<!-- 	<script type="text/javascript" src="../public/admin/js/jquery-2.1.4.min.js"></script> -->
+		<!-- 	<script type="text/javascript" src="../public/admin/js/jquery-2.1.4.min.js"></script> -->
 
 		
 
@@ -835,7 +835,7 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 		</script> -->
 
 		<!-- //amcharts -->
-	<!-- 	<script src="../public/admin/js/chart1.js"></script> -->
+		<!-- 	<script src="../public/admin/js/chart1.js"></script> -->
 		<!-- <script src="js/Chart.min.js"></script> -->
 		<script src="../public/admin/js/modernizr.custom.js"></script>
 
@@ -921,12 +921,22 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 					tableWrapper: true
 				});
 
-				$("ul.gn-submenu").slideUp('slow');
+				$("ul.gn-submenu").slideUp('fast');
 
-				$("ul.gn-menu li").hover(function () {
-					$(this).children("ul.gn-submenu").slideDown('fast');
-				}, function () {
+				$("ul.gn-menu li.employee").click(function () {
+					$(this).children("ul.gn-submenu").slideDown('slow');
+				});
+
+				$("ul.gn-menu li.employee").mouseleave(function(){
 					$(this).children("ul.gn-submenu").slideUp('slow');
+				});
+
+				$("ul.gn-menu li.product").mouseleave(function(){
+					$(this).children("ul.gn-submenu").slideUp('slow');
+				});
+
+				$("ul.gn-menu li.product").click(function () {
+					$(this).children("ul.gn-submenu").slideDown('slow');
 				});
 			});
 		</script>
@@ -966,33 +976,33 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 		<!-- <script type="text/javascript" src="../public/admin/js/bootstrap-3.1.1.min.js"></script> -->
 
 		<script src="../Public/admin/js/bootstrap-3.1.1.min.js"></script>
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
-	<script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+		<script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
 		<script type="text/javascript">
-		$(document).ready(function(){
-			$('#myTable').DataTable({
-				responsive: true,
-				"language": {
-					"lengthMenu": "Hiển thị _MENU_ số dòng trên trang",
-					"info": "Hiển thị _START_ trong tổng số _TOTAL_ dòng dữ liệu",
-					"infoEmpty": "Dữ liệu rỗng",
-					"emptyTable": "Chưa có dữ liệu nào",
-					"processing": "Đang xử lý...",
-					"search": "Tìm kiếm:",
-					"loadingRecords": "Đang load dữ liệu...",
-					"zeroRecords": "không tìm thấy dữ liệu",
-					"infoFiltered": "(Được từ tổng số _MAX_ dòng dữ liệu)",
-					"paginate": {
-						"first": "|<",
-						"last": ">|",
-						"next": ">>",
-						"previous": "<<"
-					}
-				},
-				"lengthMenu": [[2, 5, 10, 15, 20, 25, 30, -1], [2, 5, 10, 15, 20, 25, 30, "Tất cả"]]
+			$(document).ready(function(){
+				$('#myTable').DataTable({
+					responsive: true,
+					"language": {
+						"lengthMenu": "Hiển thị _MENU_ số dòng trên trang",
+						"info": "Hiển thị _START_ trong tổng số _TOTAL_ dòng dữ liệu",
+						"infoEmpty": "Dữ liệu rỗng",
+						"emptyTable": "Chưa có dữ liệu nào",
+						"processing": "Đang xử lý...",
+						"search": "Tìm kiếm:",
+						"loadingRecords": "Đang load dữ liệu...",
+						"zeroRecords": "không tìm thấy dữ liệu",
+						"infoFiltered": "(Được từ tổng số _MAX_ dòng dữ liệu)",
+						"paginate": {
+							"first": "|<",
+							"last": ">|",
+							"next": ">>",
+							"previous": "<<"
+						}
+					},
+					"lengthMenu": [[2, 5, 10, 15, 20, 25, 30, -1], [2, 5, 10, 15, 20, 25, 30, "Tất cả"]]
+				});
+				new $.fn.dataTable.FixedHeader( table );
 			});
-			new $.fn.dataTable.FixedHeader( table );
-		});
-	</script>
+		</script>
 	</body>
 	</html>
