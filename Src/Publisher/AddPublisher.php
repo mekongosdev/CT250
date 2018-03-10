958b85773a6e058b0ad20230324d9cab185157e0
Unknown <huyb1505883@student.ctu.edu.vn>
2018-03-10 10:32:46 +0700

    Validate

--- Src/Publisher/AddPublisher.php
+++ Src/Publisher/AddPublisher.php
@@ -1,3 +1,11 @@
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+<script type="text/javascript" >
+	var app = angular.module('myApp', []);
+	app.controller('myController', function($scope){
+		$scope.txtName = null;
+		$scope.txtDetails = null;
+	});
+</script>
 <?php 
 include_once("PublisherController.php");
 $name = "";
@@ -9,41 +17,49 @@ if(isset($_POST['btnAdd']))
 	addPublisher($name,$description);
 	echo '<script> alert("Thêm nhà sản xuất thành công!");</script>';
 	echo "<script>window.location.href='?page=publisher'</script>";
-	}
+}
 
-		?>
-		<div class="row">
-			<div class="col-md-12"/>
-			<form action="" method="post" accept-charset="utf-8" enctype="multipart/form-data" form-horizontal>
-				<div class="form_group">
-					<label class="control-label col-sm-12" for="email"><h2 align="center">Thêm Nhà Sản Xuất</h2></label>
-				</div>
-
-			</form>
-			<form class="form-horizontal" accept-charset="utf-8" method="post" role="form" >
-
-
-				<div class="form-group">
-					<label class="control-label col-md-2" for="txtName">Tên nhà sản xuất:</label>
-					<div class="col-sm-10">
-						<input type="text" class="form-control" id="txtName" placeholder="Nhập vào tên nhà sản xuất" name="txtName"
-						required autofocus >
-					</div>
-				</div>
-				<div class="form-group">
-					<label class="control-label col-md-2" for="txtDetails">Mô tả chi tiết:</label>
-					<div class="col-md-10">          
-
-						<textarea name="txtDetails" id="txtDetails"
-						placeholder="Nhập vào mô tả chi tiết của nhà sản xuất rượu" class="form-control" required></textarea>
-					</div>
-				</div>
-
-				<div class="form-group">        
-					<div class="col-md-offset-2 col-md-10">
-						<button type="submit" class="btn btn-info" name="btnAdd" onclick=""><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Thêm</button>
-						<button type="reset" class="btn btn-danger"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Bỏ Qua</button>
-					</div>
-				</div>
-			</form>
+?>
+<div class="row">
+	<div class="col-md-12"/>
+	<form action="" method="post" accept-charset="utf-8" enctype="multipart/form-data" form-horizontal>
+		<div class="form_group">
+			<label class="control-label col-sm-12" for="email"><h2 align="center">Thêm Nhà Sản Xuất</h2></label>
 		</div>
+
+	</form>
+	<form class="form-horizontal" accept-charset="utf-8" method="post" role="form" name="myForm" ng-app="myApp"  ng-controller="myController" novalidate="">
+
+
+		<div class="form-group">
+			<label class="control-label col-md-2" for="txtName">Tên nhà sản xuất:</label>
+			<div class="col-sm-10">
+				<input type="text" class="form-control" id="txtName" placeholder="Nhập vào tên nhà sản xuất" name="txtName"
+				required autofocus ng-model="txtName" >
+				<span style="color:red" ng-show="myForm.txtName.$dirty && myForm.txtName.$invalid">
+				<span ng-show="myForm.txtName.$error.required">Không Được Rỗng</span>
+				</span>
+			</div>
+		</div>
+		<div class="form-group">
+			<label class="control-label col-md-2" for="txtDetails">Mô tả chi tiết:</label>
+			<div class="col-md-10">          
+
+				<textarea name="txtDetails" id="txtDetails"
+				placeholder="Nhập vào mô tả chi tiết của nhà sản xuất rượu" class="form-control" required ng-model="txtDetails"></textarea>
+				<span style="color:red" ng-show="myForm.txtDetails.$dirty && myForm.txtDetails.$invalid">
+<span ng-show="myForm.txtDetails.$error.required">Không Được Rỗng</span>
+				</span>
+			</div>
+		</div>
+
+		<div class="form-group">        
+			<div class="col-md-offset-2 col-md-10">
+				<!-- <button type="submit" class="btn btn-info" name="btnAdd" onclick=""><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Thêm</button> -->
+				<input type="submit" value="Thêm Mới" name="btnAdd" class="btn btn-info" ng-disabled="myForm.txtName.$dirty && myForm.txtName.$invalid ||  
+myForm.txtDetails.$dirty && myForm.txtDetails.$invalid"/>
+				<input type="reset" name="btnReset" class="btn btn-primary">
+			</div>
+		</div>
+	</form>
+</div>
