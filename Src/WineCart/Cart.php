c4ccb5fddc6fb3a5088fea7429a0973bde8f69e3
Tuyen Nguyen <ntctuyen.ctu@gmail.com>
2018-03-17 17:48:41 +0700

    Sửa trang check out

--- Src/WineCart/Cart.php
+++ Src/WineCart/Cart.php
@@ -1,12 +1,12 @@
 <script language="javascript">
-            function confirmDelete(){
-                if(confirm("Are you sure want to delete it?")){
-                    return true;
-                }
-                else{
-                    return false;
-                }
-            }
+    function confirmDelete(){
+        if(confirm("Are you sure want to delete it?")){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 </script>
 <?php
 if (isset($_GET['action'])) {
@@ -30,47 +30,63 @@ if (isset($_POST['btnYes'])) {
 
 ?>
 
-<div class="container">
-
-<form id="form1" name="form1" method="post" action="">
-    <div class="row">
-        <div class="col-sm-3"><label>Product Names</label></div>
-        <div class="col-sm-2"><label>Publisher</label></div>
-        <div class="col-sm-2"><label>Price</label></div>
-        <div class="col-sm-2"><label>Quantity</label></div>
-        <div class="col-sm-2"><label>Total</label></div>
-        <div class="col-sm-1"><label>Delete</label></div>
+<!-- breadcrumbs -->
+<div class="breadcrumb_dress">
+    <div class="container">
+        <ul>
+            <li><a href="index.php"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Home</a> <i>/</i></li>
+            <li>Cart</li>
+        </ul>
     </div>
-    
-    <?php
-    if ($_SESSION["cart"] != null) 
-    {
-          $total = 0;
-          foreach ($_SESSION["cart"] as $key => $row) 
-          {
-    ?>
+</div>
+<!-- //breadcrumbs -->
+
+<!-- about -->
+<div class="about">
+    <div class="container"> 
+        <form id="form1" name="form1" method="post" action="">
+            <div class="row">
+                <div class="col-sm-1"><label>No.</label></div>
+                <div class="col-sm-2"><label>Product Names</label></div>
+                <div class="col-sm-2"><label>Publisher</label></div>
+                <div class="col-sm-2"><label>Price</label></div>
+                <div class="col-sm-2"><label>Quantity</label></div>
+                <div class="col-sm-2"><label>Total</label></div>
+                <div class="col-sm-1"><label>Action</label></div>
+            </div>
+            <hr/>
+            <br/>
+            <?php
+            if ($_SESSION["cart"] != null) 
+            {
+              $total = 0;
+              $i = 1;
+              foreach ($_SESSION["cart"] as $key => $row) 
+              {
+                ?>
                 <div class="row">
-                    <div class="col-sm-3"><?php echo $row['ten'] ?></div>
+                    <div class="col-sm-1"><?php echo $i++; ?></div>
+                    <div class="col-sm-2"><?php echo $row['ten'] ?></div>
                     <div class="col-sm-2"><?php echo $row["hang"] ?></div>
                     <div class="col-sm-2"><?php echo $row['gia'] ?></div>
                     <div class="col-sm-2"><input type='text' name='Wine<?php echo $key ?>' value='<?php echo $row["quantity"] ?>' size='5' style='text-align:center;' maxlength='3'/></div>
                     <div class="col-sm-2"><?php echo number_format($row["gia"] * $row["quantity"], 0, ",", ".") ?></div>
-                     <div class="col-sm-1"><a onclick='return confirmDelete()' href="./cart/del/<?php echo $key ?>"><span class="fa fa-remove"></span></a></div>
-                    </div>             
+                    <div class="col-sm-1"><a onclick='return confirmDelete()' href="./cart/del/<?php echo $key ?>"><span class="fa fa-remove fa-lg"></span></a></div>
+                </div><hr>          
                 <?php
-                        $total += $row["gia"] * $row["quantity"];
-                    }
-                    echo "<div class='row'><div class='col-sm-12' align='right'>
-                          <label>Total</label>: <span class='price'>"
-                          .number_format($total, 0, ",", ".") . "</span> VND</div></div>";
-                } else {
-                    echo "<div class='row'><div class='col-sm-12'>There are not any wine in cart</div></div>";
-                }
-                ?>
-    <div class='row'>
-        <div class='col-sm-12' align="center">
-            <input type="submit" value="Accept and check out" name="btnYes" id="btnXoa" class="InputButton"/>
-        </div>
-    </div>
-</form>
+                $total += $row["gia"] * $row["quantity"];
+            }
+            echo "<div class='row'>
+            <div class='col-sm-2 col-sm-offset-8 text-info text-right'><h3>Total: <span class='badge badge-info font-weight-bold' style='padding: 10px' >
+            $".number_format($total, 0, ",", ".") . "</span></h3></div></div><br/>";
+            echo '<div class="row">
+            <input type="submit" value="Check out" name="btnYes" id="btnXoa" class="btn btn-primary pull-right btn-lg"/>
+            </div>';
+        } else {
+            echo "<div class='row'><div class='col-sm-12'>There are not any wine in cart</div></div>";
+        }
+        ?>
+        
+    </form>
+</div>
 </div>
