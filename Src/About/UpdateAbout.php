0dd804b93cb34d73c6722be119420f33564811a8
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-03-22 19:36:49 +0700

    Update About

--- Src/About/UpdateAbout.php
+++ Src/About/UpdateAbout.php
@@ -1,39 +1,42 @@
 <div class="row">
-		<div class="col-md-12"/>
-		<form action="" method="post" accept-charset="utf-8" enctype="multipart/form-data" form-horizontal>
-			<div class="form_group">
-				<label class="control-label col-sm-12" for="email"><h2 align="center">CẬP NHẬT VỀ CHÚNG TÔI</h2></label>
-			</div>
+	<div class="col-md-12"/>
+	<form action="" method="post" accept-charset="utf-8" enctype="multipart/form-data" form-horizontal>
+		<div class="form_group">
+			<label class="control-label col-sm-12" for="email"><h2 align="center">Về Chúng Tôi</h2></label>
+		</div>
 
-		</form>
-		<form class="form-horizontal" accept-charset="utf-8" method="post" role="form" >
+	</form>
+	<form class="form-horizontal" accept-charset="utf-8" method="post" role="form" >
 
-			<div class="form-group">
-				<label class="control-label col-md-2" for="txtName">Mã Số:</label>
-				<div class="col-sm-10">
-					<input type="number" class="form-control" id="txtNum"  name="txtNum"
-					required  readonly="true" value="<?php echo $CategoryId;?>" >
-				</div>
-			</div>
-			<div class="form-group">
-				<label class="control-label col-md-2" for="txtName">Tên:</label>
-				<div class="col-sm-10">
-					<input type="text" class="form-control" id="txtName"  name="txtName"
-					required autofocus value="<?php echo $name;?>" >
-				</div>
-			</div>
-			<div class="form-group">
-				<label class="control-label col-md-2" for="txtDetails">Mô tả chi tiết:</label>
-				<div class="col-md-10">          
-					<textarea name="txtDetails" id="txtDetails" class="form-control"><?php echo $description;?></textarea>
-				</div>
-			</div>
 
-			<div class="form-group">        
-				<div class="col-md-offset-2 col-md-10">
-					<button type="submit" class="btn btn-info" name="btnUpdate" onclick=""><span class="glyphicon glyphicon-ok" aria-hidden="true"></span>Cập Nhật</button>
-					<button type="reset" class="btn btn-danger"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span>Bỏ Qua</button>
-				</div>
+		<div class="form-group">
+			<label class="control-label col-md-2" for="txtName">Tên:</label>
+			<div class="col-sm-10">
+				<input type="text" class="form-control" id="txtName" placeholder="Nhập và tên công ty rượu hiện tại" name="txtName"
+				required autofocus >
 			</div>
-		</form>
-	</div>
+		</div>
+		<div class="form-group">
+			<label class="control-label col-md-2" for="txtHistory">Lịch sử hình thành:</label>
+			<div class="col-md-10">          
+
+				<textarea name="txtHistory" id="txtHistory"
+				placeholder="Nhập vào quá trình hình thành" class="form-control" required></textarea>
+			</div>
+		</div>
+        <div class="form-group">
+			<label class="control-label col-md-2" for="slEmpl">Nhân viên tạo:</label>
+			<div class="col-md-10">          
+				<?php
+				blindListEmployye1()
+				?>
+			</div>
+		</div>
+		<div class="form-group">        
+			<div class="col-md-offset-2 col-md-10">
+				<button type="submit" class="btn btn-info" name="btnAdd" onclick=""><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Thêm</button>
+				<button type="reset" class="btn btn-danger"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Bỏ Qua</button>
+			</div>
+		</div>
+	</form>
+</div>
