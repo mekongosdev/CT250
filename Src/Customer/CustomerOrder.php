631e7f1ba6ff8b56eaa71891a86f28afafe44883
Tuyen Nguyen <ntctuyen.ctu@gmail.com>
2018-04-01 15:40:16 +0700

    add icon and print pdf

--- Src/Customer/CustomerOrder.php
+++ Src/Customer/CustomerOrder.php
@@ -1,69 +1,72 @@
 <div class="container">
-<h4 class="text-center">QUẢN LÝ ĐƠN HÀNG</h4>
-<table id="myTable" class="table-striped table-hover table-responsive">
-	<thead >
-		<tr>
-			<th><strong>STT đơn hàng</strong></th>
-			<th><strong>Tên Rượu</strong></th>
-            <th><strong>Số lượng mua</strong></th>
-			<th><strong>Ngày Đặt</strong></th>	
-			<th><strong>Ngày Nhận</strong></th>
-			<th><strong>Nơi Nhận</strong></th>
-			<th><strong>Người Nhận</strong></th>
-			<th><strong>HTTT</strong></th>
-			<th><strong>Giá Bán</strong></th>
-            <th><strong>Trại Thái</strong></th>
-            <th><strong>Hóa đơn</strong></th>
-		</tr>
-	</thead>
-    <tbody>
-<?php
+	<h4 class="text-center">QUẢN LÝ ĐƠN HÀNG</h4>
+	<table id="myTable" class="table-striped table-hover table-responsive">
+		<thead >
+			<tr>
+				<th><strong>STT đơn hàng</strong></th>
+				<th><strong>Tên Rượu</strong></th>
+				<th><strong>Số lượng mua</strong></th>
+				<th><strong>Ngày Đặt</strong></th>	
+				<th><strong>Ngày Nhận</strong></th>
+				<th><strong>Nơi Nhận</strong></th>
+				<th><strong>Người Nhận</strong></th>
+				<th><strong>HTTT</strong></th>
+				<th><strong>Giá Bán</strong></th>
+				<th><strong>Trại Thái</strong></th>
+				<th><strong>Hóa đơn</strong></th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
 
-$result = mysql_query(
-"SELECT 
-o.OrderId,w.WineName ,o.OrderCreateDate, o.OrderDeliverDate,
-o.OrderDeliverPlace, o.OrderStatus, u.FullName, 
-p.PaymentMethodName,or1.WineSoldPrice, or1.WineOrderQuantity 
-FROM `order` o
-JOIN paymentmethod p ON o.PaymentMethodId = p.PaymentMethodId
-JOIN user u ON o.Username = u.Username
-JOIN orderwinedetails or1 ON o.OrderId = or1.OrderId 
-JOIN wine w ON or1.WineOrderId = w.WineId
-LIMIT 0,25");
-; 
-$num = 1;
-while($row = mysql_fetch_array($result))
-{
-?>
-<tr>
-            <td><?php echo $num ?></td>
-            <td><?php echo $row["WineName"] ?></td>
-            <td><?php echo $row["WineOrderQuantity"] ?></td>
-            <td><?php echo $row["OrderCreateDate"] ?></td>
-            <td><?php echo $row["OrderDeliverDate"] ?></td>
-            <td><?php echo $row["OrderDeliverPlace"] ?></td>
-            <td><?php echo $row["FullName"] ?></td>
-            <td><?php echo $row["PaymentMethodName"] ?></td>
-            <td><?php echo $row["WineSoldPrice"] ?></td>
-            <td>
-            <form  method="post" action="">
-					<?php 
-						if($row["OrderStatus"]==0){
-							echo '<a class="btn btn-default" href="?page=ActiveOrder&OrderStatus='.$row["OrderStatus"].'&OrderId='.$row["OrderId"].'"><i class="fa fa-eye"></i></a>';
-						}else{
-							echo '<a class="btn btn-success" href="?page=ActiveOrder&OrderStatus='.$row["OrderStatus"].'&OrderId='.$row["OrderId"].'"><i class=" fa fa-eye-slash"></i></a>';
-						}
-					?>
-			</form>
-            </td>
-            <td>
-            <a class="btn btn-info btn" href="#"><i class="fa fa-print"></i></a>
-            </td>
-</tr>
- <?php
-$num++;
-				}
+			$result = mysql_query(
+				"SELECT 
+				o.OrderId,w.WineName ,o.OrderCreateDate, o.OrderDeliverDate,
+				o.OrderDeliverPlace, o.OrderStatus, u.FullName, 
+				p.PaymentMethodName,or1.WineSoldPrice, or1.WineOrderQuantity 
+				FROM `order` o
+				JOIN paymentmethod p ON o.PaymentMethodId = p.PaymentMethodId
+				JOIN user u ON o.Username = u.Username
+				JOIN orderwinedetails or1 ON o.OrderId = or1.OrderId 
+				JOIN wine w ON or1.WineOrderId = w.WineId
+				LIMIT 0,25");
+			; 
+			$num = 1;
+			while($row = mysql_fetch_array($result))
+			{
 				?>
-	</tbody>
-</table>
+				<tr>
+					<td><?php echo $num ?></td>
+					<td><?php echo $row["WineName"] ?></td>
+					<td><?php echo $row["WineOrderQuantity"] ?></td>
+					<td><?php echo $row["OrderCreateDate"] ?></td>
+					<td><?php echo $row["OrderDeliverDate"] ?></td>
+					<td><?php echo $row["OrderDeliverPlace"] ?></td>
+					<td><?php echo $row["FullName"] ?></td>
+					<td><?php echo $row["PaymentMethodName"] ?></td>
+					<td><?php echo $row["WineSoldPrice"] ?></td>
+					<td>
+						<form  method="post" action="">
+							<?php 
+							if($row["OrderStatus"]==0){
+								echo '<a class="btn btn-default" href="?page=ActiveOrder&OrderStatus='.$row["OrderStatus"].'&OrderId='.$row["OrderId"].'"><i class="fa fa-eye"></i></a>';
+							}else{
+								echo '<a class="btn btn-success" href="?page=ActiveOrder&OrderStatus='.$row["OrderStatus"].'&OrderId='.$row["OrderId"].'"><i class=" fa fa-eye-slash"></i></a>';
+							}
+							?>
+						</form>
+					</td>
+					<td>
+						<form method="post" action="../Src/Customer/OrderPrint.php">
+							<input type="hidden" name="OrderId" value="<?=$row['OrderId'] ?>">
+							<button type="submit" class="btn btn-info"><i class="fa fa-print"></i></button>
+						</form>
+					</td>
+				</tr>
+				<?php
+				$num++;
+			}
+			?>
+		</tbody>
+	</table>
 </div>
